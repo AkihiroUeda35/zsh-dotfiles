df38b78a7066d2842bd88b644d285cf5b8fa4bc2
Akihiro.Ueda <Akihiro.B.Ueda@sony.com>
2025-06-20 12:10:56 +0900

    _

--- install.sh
+++ install.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-set -e
-set -x
+script_dir=$(dirname "$0")
+
+HOSTNAME=$(hostname)
+if ! grep -q "$HOSTNAME" /etc/hosts; then
+    echo "⚙️  Fixing hostname resolution..."
+    echo "127.0.1.1 $HOSTNAME" | sudo tee -a /etc/hosts
+fi
 
 echo "🚀 Setting up zsh and oh-my-zsh for devcontainer..."
 sudo apt-get update
 
 echo "📦 Installing zsh..."
 sudo apt-get install -y zsh
-echo "📦 Installing oh-my-zsh..."
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-
+if [ -d "$HOME/.oh-my-zsh" ]; then
+    echo "✅ oh-my-zsh is already installed. Skipping installation..."
+else
+    echo "📦 Installing oh-my-zsh..."
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
 # Install plugins
 echo "📦 Installing plugins..."
 sudo apt-get install -y bat
@@ -21,15 +29,17 @@ git clone https://github.com/fdellwing/zsh-bat "$ZSH_CUSTOM/plugins/zsh-bat"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
 echo "⚙️  Copying .zshrc and .p10k.zsh..."    
-cp ~/dotfiles/.zshrc ~/.zshrc
-cp ~/dotfiles/.p10k.zsh ~/.zsh
-
-sudo chsh -s "$(which zsh)" "$USER"
+cp $script_dir/dotfiles/.zshrc ~/.zshrc
+cp $script_dir/dotfiles/.p10k.zsh ~/.p10k.zsh
 
 if [ -f ~/.bashrc ]; then
     echo "⚙️  Converting .bashrc environment variables for zsh..."    
-    grep '^export ' ~/.bashrc | grep -v 'shopt\|function' >> ~/.zshrc
-    grep '^alias ' ~/.bashrc >> ~/.zshrc
-    grep '^source ' ~/.bashrc >> ~/.zshrc
+    echo -e "\n# Environment variables from .bashrc\n$(grep '^export ' ~/.bashrc | grep -v 'shopt\|function')\n" >> ~/.zshrc
+    echo -e "\n# Aliases from .bashrc\n$(grep '^alias ' ~/.bashrc)\n" >> ~/.zshrc
+    echo -e "\n# Source commands from .bashrc\n$(grep '^source ' ~/.bashrc)\n" >> ~/.zshrc
 fi
-source ~/.zshrc
+
+echo "⚙️  Convert default shell with /etc/passwd"    
+current_user=$(whoami)
+zsh_path=$(which zsh)
+sudo sed -i "s|^\($current_user:[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:\)/bin/bash$|\1$zsh_path|" /etc/passwd
